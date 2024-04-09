3ed0356ce3cc175b91ed8563893758ae08606b4b
VladykaMarkiyan <144060935+VladykaMarkiyan@users.noreply.github.com>
2024-04-09 23:06:24 +0300

    Create registration.js

---
+++ registration/registration.js
@@ -0,0 +1,40 @@
+function NextPage(){
+    window.location.href= "../menu/menu.html";
+}
+
+function LogIn(){
+    let log = document.getElementById("LogIn");
+    let cr = document.getElementById("Create");
+    log.style.display = "block";
+    cr.style.display = "none";
+}
+function Reg(){
+    let log = document.getElementById("LogIn");
+    let cr = document.getElementById("Create");
+    log.style.display = "none";
+    cr.style.display = "block";
+}
+function Forgot(){
+    let log = document.getElementById("LogIn");
+    let cr = document.getElementById("Forgot");
+    log.style.display = "none";
+    cr.style.display = "block";
+}
+function OK(){
+    let log = document.getElementById("LogIn");
+    let cr = document.getElementById("OK");
+    log.style.display = "block";
+    cr.style.display = "none";
+}
+function Send(){
+    let log = document.getElementById("OK");
+    let cr = document.getElementById("Forgot");
+    log.style.display = "block";
+    cr.style.display = "none";
+}
+function Back(){
+    let log = document.getElementById("LogIn");
+    let cr = document.getElementById("Forgot");
+    log.style.display = "block";
+    cr.style.display = "none";
+}
